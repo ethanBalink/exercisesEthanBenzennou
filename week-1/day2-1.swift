6d27c5cde2184177c786e417311644965ac50692
ethanBalink <136306997+ethanBalink@users.noreply.github.com>
2023-06-12 16:51:36 +0300

    added second exercise

--- week-1/day2-1.swift
+++ week-1/day2-1.swift
@@ -1,4 +1,4 @@
-
+// exercise 1
 func sliceSize(diameter: Double?, slices: Int?) -> Double?{
  if let diameter = diameter ,let slices = slices{
 let radius = diameter / 2.0
@@ -8,4 +8,27 @@ let radius = diameter / 2.0
      return nil
  }
 }
-print(sliceSize(diameter: 16, slices: 12))
+//print(sliceSize(diameter: 16, slices: 12))
+// exercise 2
+func biggestSlice(diameterA: String, slicesA: String, diameterB: String, slicesB: String) -> String {
+    if let diameterA = Double(diameterA), let slicesAInt = Int(slicesA),
+       let diameterB = Double(diameterB), let slicesBInt = Int(slicesB) {
+        let areaA = sliceSize(diameter: diameterA, slices: slicesA)
+        let areaB = sliceSize(diameter: diameterB, slices: slicesB)
+        if let areaA = areaA {
+            if areaB == nil {
+                return "Slice A is bigger"
+            } else if let areaB = areaB, areaA > areaB {
+                return "Slice A is bigger"
+            } else if let areaB = areaB, areaA < areaB {
+                return "Slice B is bigger"
+            } else {
+                return "Neither slice is bigger"
+            }
+        } else if areaB != nil {
+            return "Slice B is bigger"
+        }
+    }
+    return "Neither slice is bigger"
+}
+//print(biggestSlice(diameterA: "10", slicesA: "6", diameterB: "12", slicesB: "8"))
