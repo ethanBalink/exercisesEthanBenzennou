4dc2fe14fd4f1542994c30bf93412344c6f91779
ethanBalink <136306997+ethanBalink@users.noreply.github.com>
2023-06-20 15:19:11 +0300

    not crashing &  not saving yet

--- week-2/todoList/todoList/ViewController.swift
+++ week-2/todoList/todoList/ViewController.swift
@@ -6,28 +6,35 @@
 //
 
 import UIKit
-
+import CoreData
 class ViewController: UIViewController ,UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
-    
+    // new code
+    // old code
     let USER_DEFAULT_KEY = "titleArr"
-    
-    var todoArr: [String] = [] {
+    // NSManagedObject
+    var todoArr: [NSManagedObject] = [] {
         didSet {
-            print(todoArr)
+            // print(todoArr)
             self.collView.reloadData()
         }
     }
     @IBOutlet weak var collView: UICollectionView!
     // save data in userdefaults
     let defaults = UserDefaults.standard
-    
+    var context: NSManagedObjectContext!
+    var entity: NSEntityDescription!
     override func viewDidLoad() {
         super.viewDidLoad()
-        if var memoryArray = UserDefaults.standard.array(forKey: USER_DEFAULT_KEY) as? [String] {
-            todoArr = memoryArray
-        } else {
-            todoArr = ["eat breakfast", "learn"]
-        }
+        //old code
+        //        if var memoryArray = UserDefaults.standard.array(forKey: USER_DEFAULT_KEY) as? [String] {
+        //            todoArr = memoryArray
+        //        } else {
+        //            todoArr = ["eat breakfast", "learn"]
+        //        }
+        // new code
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        context = appDelegate.persistentContainer.viewContext
+        entity = NSEntityDescription.entity(forEntityName: "Todo", in: context)
     }
     // row size return
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -39,8 +46,8 @@ class ViewController: UIViewController ,UICollectionViewDataSource, UICollection
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionviewCell", for: indexPath) as! CellCollectionViewCell
         cell.backgroundColor = UIColor.cyan
-        let  text = todoArr[indexPath.item]
-        cell.label.text = text
+        let currentTodo = todoArr[indexPath.item]
+        cell.label.text = currentTodo.value(forKey: "title" ) as? String
         return cell
     }
     
@@ -57,8 +64,14 @@ class ViewController: UIViewController ,UICollectionViewDataSource, UICollection
     
     // func when add item
     func addTask(task:String) {
-        todoArr.append(task)
-        UserDefaults.standard.setValue(todoArr, forKey: USER_DEFAULT_KEY)
+        // todoArr.append(task)
+        // old code
+        // UserDefaults.standard.setValue(todoArr, forKey: USER_DEFAULT_KEY)
+        //new code
+        let newTodo = NSManagedObject(entity: entity!, insertInto: context)
+        newTodo.setValue(task, forKey: "title")
+        todoArr.append(newTodo)
+        
     }
     
     // validation
@@ -67,8 +80,22 @@ class ViewController: UIViewController ,UICollectionViewDataSource, UICollection
         let inputPred = NSPredicate(format: "SELF MATCHES %@", inputRegEx)
         return inputPred.evaluate(with: input)
     }
-    
-    
+    // new code
+    func getData(){
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Todo")
+        request.returnsObjectsAsFaults = false
+        do {
+            let result = try context.fetch(request)
+            for data in result as! [NSManagedObject] {
+                print(data.value(forKey: "title") as! String)
+            }
+            
+        } catch {
+            
+            print("Failed")
+        }
+    }
+    //
     @IBAction func addItem(_ sender: UIBarButtonItem) {
         // defines alert
         let dialogMessage = UIAlertController(title: "Add Task", message: "Write the task you need to do", preferredStyle: .alert)
@@ -77,7 +104,7 @@ class ViewController: UIViewController ,UICollectionViewDataSource, UICollection
         // adds button and action
         let ok = UIAlertAction(title: "Add", style: .default, handler: { (action) -> Void in
             if let userInput = dialogMessage.textFields?.first?.text {
-                print(userInput)
+                //  print(userInput)
                 if self.isValidInput(userInput) {
                     self.addTask(task: userInput)
                 }}
